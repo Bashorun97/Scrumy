b3e77af9e138f5aa646f38170c92a00bec3d26a0
bashorun <bashorun.emma@gmail.com>
2020-01-17 11:29:31 +0000

    lab 5 works at this commit

--- myscrumy/myscrumy/settings.py
+++ myscrumy/myscrumy/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ajy8#vjy#n$bh*4yixg-4k51vmbc@w$9j_#lpoi9qe*rhckf!('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['18.218.63.60']
 
 # Application definition
 
