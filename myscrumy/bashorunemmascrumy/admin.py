084d1a58bc792f66758c33f3fa3e4832cb170a2a
Bashorun97 <bashorun.emma@gmail.com>
2020-01-17 21:32:37 +0100

    Django Models

--- myscrumy/bashorunemmascrumy/admin.py
+++ myscrumy/bashorunemmascrumy/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import ScrumyGoals, ScrumyHistory, GoalStatus
+
+admin.site.register(ScrumyGoals)
+admin.site.register(ScrumyHistory)
+admin.ste.register(GoalStatus)
--- myscrumy/bashorunemmascrumy/models.py
+++ myscrumy/bashorunemmascrumy/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
 
 # Create your models here.
+class ScrumyGoals(models.Model):
+    goal_name = models.CharField(max_length=50)
+    goal_id = models.IntegerField(max_length=50)
+    created_by = models.CharField(max_length=50)
+    moved_by = models.CharField(max_length=50)
+    owner = models.CharField(max_length=50)
+    goal_status = models.ForeignKey(GoalStatus, on_delete=models.PROTECT)
+    user = models.ForeignKey(User, related_name="user_goal", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.goal_name
+
+class ScrumyHistory(models.Model):
+    moved_by = models.CharField(max_length=50)
+    created_by = models.CharField(max_length=50)
+    moved_from = models.CharField(max_length=50)
+    moved_to = models.CharField(max_length=50)
+    time_of_action = models.DateField()
+    goal = models.ForeignKey(ScrumyGoals, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.created_by
+
+class GoalStatus(models.Model):
+    status_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.status_name
