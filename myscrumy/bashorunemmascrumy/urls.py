9944f5702ae5473c054f582dcd72bffa2caf0d82
bashorun <bashorun.emma@gmail.com>
2020-01-24 14:32:24 +0000

    lab 10 works on this commit

--- myscrumy/bashorunemmascrumy/urls.py
+++ myscrumy/bashorunemmascrumy/urls.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 urlpatterns = [
-        path('', views.get_grading_parameters),
+       # path('', views.get_grading_parameters),
+        path('', views.query_filter),
 ]
--- myscrumy/bashorunemmascrumy/views.py
+++ myscrumy/bashorunemmascrumy/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import ScrumyGoals, ScrumyHistory, User, GoalStatus
 
 # Create your views here.
+'''
 def get_grading_parameters(request):
     return HttpResponse("This is a Scrum Application")
+'''
+def query_filter(request):
+    name = ScrumyGoals.objects.filter(goal_name="Learn Django")
+    return HttpResponse(name)
